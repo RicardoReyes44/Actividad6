eb1336942e357ef0443ac5b3f1cfc87d4e96abe6
RicardoReyes44 <rickyboy1002@gmail.com>
2020-09-30 18:50:08 -0500

    Se agrego el menu y manejo de excepciones en la clase 'Prueba'

--- src/RegistroAspirantes.java
+++ src/RegistroAspirantes.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 
 public class RegistroAspirantes {
 	
+    ArrayList<Aspirante> listaAspirantes;
 	
+	public RegistroAspirantes() {listaAspirantes = new ArrayList<Aspirante>();}
 	
 }
