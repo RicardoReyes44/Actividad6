36f83936cf55b55758b3e5ae99da0a62304092ce
RicardoReyes44 <rickyboy1002@gmail.com>
2020-09-30 16:48:42 -0500

    Se agregaron constructores y se sobreescribio el metodo 'toString' en la clase 'Aspirante' dentro del archivo 'Aspirante'

--- src/Aspirante.java
+++ src/Aspirante.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 class Aspirante{
 	
@@ -5,5 +6,19 @@ class Aspirante{
 	private String nombre;
 	private byte edad;
 	private String redesSociales[];
+	
+	public Aspirante() {}
+	public Aspirante(String nombre, byte edad, String[] redesSociales) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.redesSociales = redesSociales;
+	}
+	
+	@Override
+	public String toString() {
+		return "Aspirante [nombre=" + nombre + ", edad=" + edad + ", redesSociales=" + Arrays.toString(redesSociales)
+				+ "]";
+	}
 
 }//class Aspirante
--- src/Prueba.java
+++ src/Prueba.java
@@ -3,15 +3,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 
-class Aspirante{
-	
-	//folio
-	private String nombre;
-	private byte edad;
-	private String redesSociales[];
-
-}//class Aspirante
-
 public class Prueba {
 
 	public static void main(String[] args) {
