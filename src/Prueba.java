74d0163424773b1daed93925ac72ad686f8230dc
RicardoReyes44 <rickyboy1002@gmail.com>
2020-09-30 20:41:15 -0500

    Se agrego el metodo de 'ingresarAspirante' en la clase 'RegistroAspirantes'

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,13 +1,45 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Prueba {
 
 	public static void main(String[] args) {
 		
+		RegistroAspirantes ra = new RegistroAspirantes();
+		Scanner entrada = new Scanner(System.in);
+		boolean candado = true;
+
 		
+		while(candado) {
+			try {
+			    System.out.println("1.- Registrar aspirante");
+			    System.out.println("2.- Mostrar aspirantes");
+			    System.out.println("3.- Salir");
+			    System.out.print("Introduce opcion: ");
+			    int opcion = entrada.nextInt();
+			
+			    switch(opcion) {
+			    case 1:
+				    break;
+			    case 2:
+				    break;
+			    case 3:
+				    candado = false;
+				    break;
+			    default:
+				    System.out.println("Opcion invalida, por favor vuelve intentarlo");
+				    break;
+			    }
+			}catch(InputMismatchException error) {
+				System.out.println("Error de entrada<" + error + "> por favor vuelve a intentarlo");
+				entrada.nextLine();
+			}
+			
+		}
+		System.out.println("X.X");
 		
 	}
 
--- src/RegistroAspirantes.java
+++ src/RegistroAspirantes.java
@@ -1,9 +1,45 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RegistroAspirantes {
 	
     ArrayList<Aspirante> listaAspirantes;
+    Scanner entrada = new Scanner(System.in);
 	
 	public RegistroAspirantes() {listaAspirantes = new ArrayList<Aspirante>();}
+	
+	public void ingresarAspirante() {
+		
+		String[] redes;
+		byte edad;
+		
+		System.out.println("Introduce nombre: ");
+		String nombre = entrada.nextLine();
+
+		System.out.println("Introduce redes sociales, ej: Facebook, Twitter, instagram");
+		redes = entrada.nextLine().replace(" ", "").split(",");
+
+		while(true) {
+			System.out.println("Introduce edad: ");
+			edad = entrada.nextByte();
+			
+			if(edad>17) {
+				break;
+			}else {
+				System.out.println("No puedes ingresar esa edades menores\nde 18,por favor prueba de nuevo");
+			}
+		}
+		
+		listaAspirantes.add(new Aspirante(nombre, (byte)edad, redes));
+		
+	}
+	
+	public void eliminarAspirante() {
+		
+	}
+	
+	public void mostrarAspirantes() {
+		
+	}
 	
 }
